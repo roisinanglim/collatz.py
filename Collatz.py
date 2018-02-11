4504645f151e50fc528fcd731daa42ae2fd5e271
roisinanglim <36383923+roisinanglim@users.noreply.github.com>
2018-02-11 21:46:10 +0000

    Update Collatz.py

--- Collatz.py
+++ Collatz.py
@@ -1,13 +1,16 @@
+#Roisin Anglim 11-02-18 
+#Collatz : If even number divide by 2, if uneven multiply by 3 + 1
+
 # Requests user to enter an interger
 i = int(input("input integer ")) 
 print(i) 
-#Do this when I is not equal to one
+#Do this when i is not equal to one
 while i != 1: 
-#When I even number
+#When i even number
      if i %2 ==0: 
          i= i/2
          print(int(i))   
-#When I uneven number      
+#When i uneven number      
      elif i %2 ==1:
          i= i*3+1
          print(int(i))
